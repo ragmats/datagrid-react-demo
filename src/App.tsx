461cf1f73e9cdb08a5d1d20b3c700db98d7b28d0
Steven Coy <stevencoy@gmail.com>
2024-09-02 22:56:56 -0700

    Changed basename

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Calendar from "./components/Calendar";
 import AddOrChangeDelivery from "./components/AddOrChangeDelivery";
 
 // TODO Remove sorting, sort by Ful Date + Delivery ID
-// TODO Deploy to Github Pages
 
 // "Done":
 // Add "Add" button
